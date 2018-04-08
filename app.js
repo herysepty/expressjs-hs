9bb8a7f2446eec66820bcd581e19b85750473df5
herysepty <herysepty6@gmail.com>
2018-04-08 14:52:09 +0700

    Added Route

--- app.js
+++ app.js
@@ -8,7 +8,29 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// hs start
+//Databases mysql
+var mysql = require('mysql');
+/**
+ * This middleware provides a consistent API
+ * for MySQL connections during request/response life cycle
+ */
+var conn  = require('express-myconnection');
+/**
+ * Store database credentials in a separate config.js file
+ * Load the file/module and its values
+ */
+var config = require('./config')
+var db_conf = {
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    port: config.database.port,
+    database: config.database.db
+}
+app.use(conn(mysql, db_conf, 'pool'));
 
+//end hs
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -20,7 +42,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
+// HS api endpoint
+app.use('/api/v1/users', usersRouter.list);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +62,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+//hs listen
+app.listen(3000, function(){
+    console.log('Server running at port 3000: http://127.0.0.1:3000')
+})
 module.exports = app;
