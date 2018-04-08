955084c0d1c2494a6f707c43bdf42c59f12ef754
herysepty <herysepty6@gmail.com>
2018-04-08 14:51:16 +0700

    config

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'asdasd',
+        port: 3306,
+        db: 'hs_we'
+    },
+    server: {
+        host: '127.0.0.1',
+        port: '3000'
+    }
+}
+
+module.exports = config
