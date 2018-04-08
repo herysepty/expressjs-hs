ca82c7052b20de30e0718def97484a1ae90e8155
herysepty <herysepty6@gmail.com>
2018-04-08 14:52:54 +0700

    Routes users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource users hs');
+  res.send('respond with a resource');
 });
 
+router.list = function(req, res){
+
+    req.getConnection(function(err,connection){
+
+        var query = connection.query('SELECT * FROM users   ',function(err,rows)
+        {
+
+            if(err) {
+
+                console.log("Error Selecting : %s ",err );
+            } else {
+
+                res.send(rows);
+            }
+        });
+    });
+};
 module.exports = router;
